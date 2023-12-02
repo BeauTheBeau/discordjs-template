1b7bbf6a092032e9711917a9fa7c217b0a9d0fd0
BeauTheBeau <beauthebeau@proton.me>
2023-12-02 16:59:57 +0000

    feat: Create new eventHandler.js

---
+++ src/handlers/eventHandler.js
@@ -0,0 +1,39 @@
+"use strict";
+
+require("dotenv").config();
+const eventLogger = require("bunyan").createLogger({ name: "eventHandler" });
+const path = require("path");
+const { readdirSync } = require("fs");
+const fs = require("fs");
+
+module.exports = async (client) => {
+  const eventPath = path.join(__dirname, "../events");
+
+  ensureEventDirectoryExists(eventPath);
+
+  const eventFiles = retrieveEventFilesFromDirectory(eventPath);
+
+  if (eventFiles.length > 0) registerEvents(client, eventFiles);
+  else eventLogger.warn("No events found.");
+};
+
+const ensureEventDirectoryExists = (eventPath) => {
+  if (fs.existsSync(eventPath))
+    return eventLogger.info("Events directory exists.");
+  eventLogger.warn("Events directory does not exist. Creating it now...");
+  fs.mkdirSync(eventPath);
+};
+
+const retrieveEventFilesFromDirectory = (eventPath) => {
+  const eventFiles = readdirSync(eventPath);
+  return eventFiles.filter((file) => file.endsWith(".js"));
+};
+
+function registerEvents(client, eventFiles) {
+  for (const file of eventFiles) {
+    const event = require(path.join(__dirname, "../events", file));
+    const eventName = file.split(".")[0];
+    eventLogger.info(`Registered event ${eventName}`);
+    client.on(eventName, event.bind(null, client));
+  }
+}
--- src/handlers/initHandlers.js
+++ src/handlers/initHandlers.js
@@ -3,18 +3,11 @@
  * @description Initializes handlers, such as commands abd events.
  */
 
-// Import dependencies
-const fs = require('fs');
-const path = require('path');
-const config = require('../../config.js');
-
 // Import handlers
-const loadCommands = require('./commandHandler.js');
-const loadEvents = require('./eventHandler.js');
+const loadCommands = require("./commandHandler.js");
+const loadEvents = require("./eventHandler.js");
 
 module.exports = async (client) => {
-
-    await loadCommands(client);
-
-}
-
+  await loadCommands(client);
+  await loadEvents(client);
+};
