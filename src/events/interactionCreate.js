22784478d7c59518c88a8d77e49c078ea76c8cd9
beauthebeau <beau@beauthebeau.pro>
2023-12-14 18:46:25 +0000

    feat: Add blacklisting users & servers by ID

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,8 +1,25 @@
 const eventLogger = require("bunyan").createLogger({ name: "eventHandler" });
+const config = require("./../../config");
 
 module.exports = {
   name: "interactionCreate",
   async execute(client, interaction) {
+    // Check if the guild/user is blacklisted
+    if (config.blacklist.enabled) {
+      if (config.blacklist.users.includes(interaction.user.id)) {
+        return interaction.reply({
+          content: "You are blacklisted from using this bot",
+          ephemeral: true,
+        });
+      }
+      if (config.blacklist.guilds.includes(interaction.guild.id)) {
+        return interaction.reply({
+          content: "This guild is blacklisted from using this bot",
+          ephemeral: true,
+        });
+      }
+    }
+
     if (interaction.isCommand()) {
       const command = client.commands.get(interaction.commandName);
       if (!command)
